61a7c361df48335faa9a18439d56738a458b1692
student <student@DESKTOP-PCTLA0M>
2016-12-14 11:53:43 -0400

    change page title to sql chapters

--- includes/services.php
+++ includes/services.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h2 class="pageTitle">SQL Demos</h2>
+                <h2 class="pageTitle">SQL Chapters</h2>
             </div>
         </div>
     </div>
@@ -14,7 +14,7 @@
         <div class="row"> 
             <div class="col-md-12">
                 <div class="about-logo">
-                    <h3>SQL <span class="color">Demos</span></h3>
+                    <h3>SQL <span class="color">Chapters</span></h3>
                     <p>For the months of October and November, SQL queries where covered in class. A list of the chapters will be excusable along with the demos that went along with them.  
                         <br>!!!!!!!!!!!Just to make it look better I would like to put another blerb here about SQL to fill up the space  sgv drbvfdgvrefgv fdgvfdcv fbfdhuhvlksjlkjfdo!!!!!!!!!!!!!</p>
                 </div>  
