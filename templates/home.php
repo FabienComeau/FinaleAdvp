163afbc08616db1bba38cf01643421139755e68c
student <student@DESKTOP-PCTLA0M>
2016-12-16 13:20:45 -0400

    put in a back button on the democode page and work on the about us page

--- templates/home.php
+++ templates/home.php
@@ -2,14 +2,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<h2 class="pageTitle">We Offer</h2>
+				<h2 class="pageTitle">Web Programming</h2>
 			</div>
 		</div>
 	</div>
 	</section>
 <section id="home">
   <div class="banner-container"> 
-      <img src="./img/SQL2.png" alt=""/>
+      <img src="./img/webprog1.jpg" alt=""/>
     <div class="container banner-content">
 
     </div>
